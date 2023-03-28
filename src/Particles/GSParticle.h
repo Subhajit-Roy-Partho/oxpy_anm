88e35a3eb0558bd4e183e246b5347cc34538c294
Subhajit Roy <40741607+Subhajit-Roy-Partho@users.noreply.github.com>
2023-03-27 22:55:51 -0700

    commentry

--- src/Particles/GSParticle.h
+++ src/Particles/GSParticle.h
@@ -13,17 +13,18 @@
  * @brief A customisable particle. Used by CGDNAInteraction.
  * A literal replica of ANMParticle
  */
+
 class GSParticle: public BaseParticle {
 protected:
 
 public:
     GSParticle();
-    virtual ~GSParticle();
+    virtual ~GSParticle(); //
 
     virtual bool is_rigid_body() { return false; }
 
-    virtual bool is_bonded(BaseParticle *q);
-    virtual void add_bonded_neighbor(BaseParticle *nn);
-    std::vector<int> bonded_neighs;
+    virtual bool is_bonded(BaseParticle *q); //Returns if a particle is bonded or not bool
+    virtual void add_bonded_neighbor(BaseParticle *nn); //To add neighbour connected particles by populating bonded_neighs vector.
+    std::vector<int> bonded_neighs; //Contains the connected particles.
 };
 #endif //GS_PARTICLE_H
